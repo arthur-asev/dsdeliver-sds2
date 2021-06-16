abfd5230f14a3510c07b051ad02e73647303560b
Arthur Rodrigues <arthurginho02@gmail.com>
2021-06-16 16:33:17 -0300

    troca dos links do footer

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,21 +1,17 @@
 import './styles.css';
-import { ReactComponent as YouTubeIcon } from '../images/Youtube.svg';
 import { ReactComponent as LinkedinIcon } from '../images/Linkedin.svg';
-import { ReactComponent as InstagramIcon } from '../images/Instagram.svg';
+import {ReactComponent as GitIcon} from '../images/Git.svg';
 
 function Footer(){
     return(
            <footer className="main-footer">
                App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
                <div className="footer-incons">
-                   <a href="https://www.linkedin.com/school/devsuperior/" target="_new">
+                   <a href="https://www.linkedin.com/in/arthur-rodrigues-de-asevêdo-9aa261191/" target="_new">
                     <LinkedinIcon/>
                    </a>
-                   <a href="https://www.instagram.com/devsuperior.ig/" target="_new">
-                   <InstagramIcon/>
-                   </a>
-                   <a href="https://www.youtube.com/devsuperior" target="_new">
-                   <YouTubeIcon/>
+                   <a href="https://github.com/arthur-asev" target="_new">
+                    <GitIcon/>
                    </a>
                </div>
            </footer>
